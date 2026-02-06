d299cb5ffa080f804d2495ca49b244e5c17c8dc3
Patrick Jones <forksofpower@gmail.com>
2026-02-06 03:03:27 -0600

    add parentheses to contants

--- eval-expr/constants.py
+++ eval-expr/constants.py
@@ -1,5 +1,15 @@
+TERMINATOR = "\0"
 
-# Helper class for operator symbols to make sure I don't get my wires crossed
+
+# Helper classes for operators & symbols to make sure I don't get my wires crossed
+class Bracket:
+    OPEN: str
+    CLOSE: str
+
+class Parenthesis(Bracket):
+    OPEN = '('
+    CLOSE = ')'
+
 class Operator:
     ADD = '+'
     SUBTRACT = '-'
