35d067465e10ee3b75bccedc0084080d892c8d9b
Patrick Jones <forksofpower@gmail.com>
2026-02-06 03:02:53 -0600

    refactor` eval_expr` to use a recursive handler

--- eval-expr/eval_expr.py
+++ eval-expr/eval_expr.py
@@ -1,64 +1,58 @@
-import re
-from constants import Operator
+from constants import Operator, Parenthesis, TERMINATOR
 
-TERMINATOR = "\0"
-
-def eval_expr(expression: str):
-    stack = []
-    current_number = 0
-    previous_operator = Operator.ADD
-    index = 0
-    
+def eval_expr(expression: str) -> float:
     # format expression, end with dummy terminator to denote end of string
-    expression = expression.lower().replace(" ", "").replace("x", "*") + TERMINATOR 
+    expression = expression.lower().replace(" ", "").replace("x", "*") + TERMINATOR
 
-    while index <= len(expression) - 1:
-        char = expression[index]
-
-        # handle digits
-        if char.isdigit():
-            current_number = (current_number * 10) + int(char)
+    def sum_stack(s: [int]):
+        return float(sum(s))
         
-        # handle parenthetical sub-expressions
-        elif char == '(':
-            sl = 0
-            depth = 0
-            # dig into expression until closing paren is found
-            for i, c in enumerate(expression[index:]):
-                sl = i
-                if c == "(":
-                    depth += 1
-                elif c == ")" and depth > 0:
-                    depth -= 1
-                if c == ")" and depth == 0:
-                    break
-            end_index = index + sl
-            # evaluate sub-expression
-            se = expression[index+1 : end_index]
-            current_number = eval_expr(se)
-            # compensate index for dig depth
-            index = end_index
+    # recursive handler
+    def _eval_expr_handler(index: int) -> (float, int):
+        stack = []
+        current_number = 0
+        previous_operator = Operator.ADD
 
-        # handle operators
-        elif not char.isdigit() or index == len(expression):
-            # handle add/subtract out-of-band
-            if previous_operator == Operator.ADD:
-                stack.append(current_number)
-            elif previous_operator == Operator.SUBTRACT:
-                stack.append(-current_number)
-            # handle multiply/divide in-band to respect order of operations
-            elif previous_operator == Operator.MULTIPLY:
-                prev_number = stack.pop()
-                stack.append(prev_number * current_number)
-            elif previous_operator == Operator.DIVIDE:
-                prev_number = stack.pop()
-                stack.append(prev_number / current_number)
+        while index <= len(expression) - 1:
+            char = expression[index]
 
-            # reset for next number/operator
-            previous_operator = char
-            current_number = 0
-        
-        # continue to next character
-        index += 1
+            # handle digits
+            if char.isdigit():
+                current_number = (current_number * 10) + int(char)
+            
+            # handle open paren
+            elif char == Parenthesis.OPEN:
+                # recurse on rest of expression starting at index + 1
+                new_number, new_index = _eval_expr_handler(index + 1)
+                current_number = new_number
+                index = new_index
 
-    return float(sum(stack))
+            # handle operators
+            elif not char.isdigit() or index == len(expression):
+                # handle add/subtract out-of-band
+                if previous_operator == Operator.ADD:
+                    stack.append(current_number)
+                elif previous_operator == Operator.SUBTRACT:
+                    stack.append(-current_number)
+
+                # handle multiply/divide in-band to respect order of operations
+                elif previous_operator == Operator.MULTIPLY:
+                    stack.append(stack.pop() * current_number)
+                elif previous_operator == Operator.DIVIDE:
+                    stack.append(stack.pop() / current_number)
+                
+                # handle close paren, return immediately
+                if char == Parenthesis.CLOSE:
+                    return sum_stack(stack), index
+
+                # reset for next number/operator
+                previous_operator = char
+                current_number = 0
+            
+            # continue to next character
+            index += 1
+        return sum_stack(stack), index
+
+    # run the evaluation
+    result, _ = _eval_expr_handler(0) # start parsing at the first character
+    return result
